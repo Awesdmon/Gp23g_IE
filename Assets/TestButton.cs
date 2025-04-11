6e409fba3c03927e7452cd0e67a28c6284fb3524
Samson Shields <user@FIH-COM23a104.fsu.edu>
2025-04-11 12:59:23 -0400

    Saving version

---
+++ Assets/TestButton.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButton : MonoBehaviour
+{
+    public Transform[] potentialParents;
+ public GameObject child;
+ public int counter;
+    public Transform position1;
+    public Transform position2;
+    public Transform position3;
+    public Transform position4;
+    public Transform position5;
+    public Transform position6;
+    public Transform position7;
+    public Transform position8;
+    public Transform position9;
+    public Transform position10;
+    public Transform position11;
+    public Transform position12;
+    public Transform position13;
+    public Transform position14;
+    public Transform position15;
+    public Transform position16;
+    public Transform position17;
+    public Transform position18;
+    public Transform position19;
+    public Transform position20;
+    public Transform position21;
+    public Transform position22;
+    public Transform position23;
+    public Transform position24;
+    public Transform position25;
+    public Transform position26;
+    public Transform position27;
+    public void Start()
+    {
+        potentialParents = new Transform[] {position1, position2 , position3 , position4 , position5 , position6 , position7 , position8 , position9 , position10, position11, position12, position13, position14, position15, position16, position17, position18, position19, position20, position21, position22, position23, position24, position25, position26, position27 };
+        Transform randomParent = potentialParents[Random.Range(0, potentialParents.Length)];
+        transform.SetParent(randomParent);
+        //transform.localScale = Vector3.one;
+        counter++;
+    }
+
+
+
+
+
+
+
+
+
+    /*public void Test(Transform[] potentialParents)
+    {
+        Transform randomParent = potentialParents[Random.Range(0, potentialParents.Length)];
+        transform.SetParent(randomParent);
+        transform.localScale = Vector3.one;
+        counter++;
+    }*/
+}
